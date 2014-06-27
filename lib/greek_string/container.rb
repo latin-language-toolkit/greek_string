a0f0c6c5d71db3bea55cc8b8782b2459a4e22d05
lichtr <robert.lichtensteiner@gmail.com>
2014-06-28 01:41:02 +0200

    Add method missing

--- lib/greek_string/container.rb
+++ lib/greek_string/container.rb
@@ -13,17 +13,27 @@ class GreekString
       @container.flat_map(&:letters)
     end
 
-    def vowels
-      @container.flat_map(&:vowel)
+    def to_a
+      @container
+    end
+
+    def to_s(type)
+      @container.flat_map(&type)
+    end
+
+    def methods(meth)
+      @container.map! do |letter|
+        if letter.instance_variable_defined?("@" + meth.to_s)
+          letter
+        end
+      end
+      @container.compact
       self
     end
 
-    def upper
-      @container.map(&:upper)
-    end
-
-    def to_a
-      @container
+    def method_missing(meth)
+      blk = Proc.new { methods(meth) }
+      self.class.send(:define_method, meth, &blk)
     end
 
   end
