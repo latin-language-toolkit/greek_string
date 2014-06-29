ddc5ac1d9ed9d025494ffd45518f6a3e565ef7ec
lichtr <robert.lichtensteiner@gmail.com>
2014-06-29 22:59:26 +0200

    Major rebuild
    Each letter is now nested in a class hierarchy: Letter is the superclass
    of a letter class (e.g. Alpha), which has child classes for each letter
    type. (e.g. Plain, Asper, AsperIota...). Upper and lower case are
    instance variables.

--- lib/greek_string.rb
+++ lib/greek_string.rb
@@ -5,62 +5,36 @@ class GreekString
   require 'greek_string/letter'
   require 'greek_string/container'
 
-  attr_reader :letters
   def initialize
     l = File.open('./data/gr_letters.json', 'r') { |f| f.read }
     @json = JSON.parse(l)
-    @letters = GreekString::Container.new
-    @res = []
+    GreekString.initiate_container
     create_letter_objects
   end
 
-  def create_letter_objects
-    @json.each do |letter, obj|
-      @letters << GreekString::Letter.new(Hash[letter, obj])
-    end
-    require 'pry'; binding.pry
-  end
-
-  # returns all greek letters upper and lower case
-  def all
-    @letters.letters
-  end
-
-  def all_plain_upper
-    @letters.upper
-  end
-  # possible values for args: vowel, consonants, double_consonants,
-  # returns upper and lower case
-  def find(type)
-    res = @json.flat_map do |name, obj|
-      if obj["type"] == type
-        obj["letters"]
-      end
-    end
-    res.compact
-  end
-
-  def find_lower_case
-    find_key(@json, "lower")
-  end
-
-  def find_upper_case
-    find_key(@json, "upper")
+  def letters
+    GreekString.letters
   end
 
   private
 
-  # finds a given key in a nested hash and returns its string value.
-  # Keeps on searching if value is a hash
-  def find_key(hsh, key)
-    hsh.each do |k, value|
-      if k == key
-        @res << value
-        #require 'pry'; binding.pry
-      elsif value.is_a? Hash
-        find_key(value, key)
-      end
+  def self.initiate_container
+    @letters = GreekString::Container.new
+  end
+
+  def self.letters
+    @letters
+  end
+
+  def class_hierarchy
+    GreekString::Letter
+  end
+
+  def create_letter_objects
+    @json.each do |letter, obj|
+      klass = class_hierarchy.const_set(letter.capitalize, Class.new(class_hierarchy))
+      klass.new(Hash[letter, obj])
     end
-    @res
+    require 'pry'; binding.pry
   end
 end
--- lib/greek_string/letter.rb
+++ lib/greek_string/letter.rb
@@ -2,7 +2,9 @@ class GreekString
   class Letter
     def initialize(letter_hsh)
       @hash = letter_hsh
-      create_inst_var(@hash[name])
+      @inner_hsh = @hash[name]
+      create_inst_var(@inner_hsh)
+      create_class
     end
 
     def name
@@ -14,10 +16,43 @@ class GreekString
     def create_inst_var(hsh, outer_key=nil)
       if hsh.is_a? Hash
         hsh.keys.each do |var|
-          inst_var_name = create_inst_var_name(var, outer_key)
-          instance_variable_set("@#{inst_var_name}", hsh[var])
-          self.class.class_eval("attr_accessor :#{inst_var_name}")
-          create_inst_var(hsh[var], var)
+          if outer_key =~ /plain|diacritics/
+            next
+          else
+            inst_var_name = create_inst_var_name(var, outer_key)
+            instance_variable_set("@#{inst_var_name}", hsh[var])
+            self.class.class_eval("attr_accessor :#{inst_var_name}")
+            create_inst_var(hsh[var], var)
+          end
+        end
+      end
+    end
+
+    def class_hierarchy
+      GreekString::Letter.const_get(name.capitalize)
+    end
+
+    def create_class
+      letter_types = { "plain" => @inner_hsh["plain"]}.merge(to_be_merged)
+      letter_types.each do |type, hsh|
+      klass = class_hierarchy.const_set(camel_case(type), klass_body)
+        GreekString.letters << klass.new(hsh, type)
+      end
+    end
+
+    def to_be_merged
+      @inner_hsh["diacritics"] || {}
+    end
+
+    def camel_case(string)
+      string.split("_").map { |el| el.capitalize }.join
+    end
+
+    def klass_body
+      Class.new(class_hierarchy) do
+        def initialize(hash, type)
+          instance_variable_set("@#{type}", true)
+          create_inst_var(hash)
         end
       end
     end
